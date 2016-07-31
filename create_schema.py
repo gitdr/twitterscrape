4410fb5e59465fdc45e0c9a7ca809d62566412a1
Your Name <you@example.com>
2016-07-31 01:26:08 +0000

    implement basic insertion of user/tweet into DB

--- create_schema.py
+++ create_schema.py
@@ -7,27 +7,23 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
  
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
  
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Tweet(Base):
+    __tablename__ = 'tweet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    tweet_text = Column(String(250))
+    # street_number = Column(String(250))
+    # post_code = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
  
 # Create an engine that stores data in the local directory's
 # sqlalchemy_example.db file.
-engine = create_engine('sqlite:///sqlalchemy_example.db')
+engine = create_engine('sqlite:///tweets.db')
  
 # Create all tables in the engine. This is equivalent to "Create Table"
 # statements in raw SQL.
--- walk.py
+++ walk.py
@@ -1,8 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+ 
+from create_schema import Base, User, Tweet
+
 import os
 import sys
 from bs4 import BeautifulSoup
 from bs4 import NavigableString
 import pprint
+
+engine = create_engine('sqlite:///tweets.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+ 
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
 
 # rootdir = sys.argv[1]
 
@@ -41,6 +61,12 @@ def parse_tweet_li(li):
 
     return data 
 
+
+users = [user.id for user in session.query(User).all()]
+tweets_in_db = [tweet.id for tweet in session.query(Tweet).all()]
+
+print users
+
 for folder, subs, files in os.walk("./data"):
   if files:
     f = open(folder + '/' + files[0], 'r')
@@ -56,9 +82,31 @@ for folder, subs, files in os.walk("./data"):
     amt = 0
 
     for li in lis:
-      pp.pprint(parse_tweet_li(li))
+      tweet = parse_tweet_li(li)
+      pp.pprint(tweet)
       print "-===========================-"
       amt += 1
+      user_id = int(tweet['data-user-id'])
+      if not user_id in users:
+        # Insert a Person in the person table
+        new_user = User(id=tweet['data-user-id'], name=tweet['data-name'])
+        session.add(new_user)
+        users.append(user_id)
+        session.commit()
+      else:
+        user = session.query(User).filter(User.id == user_id).first()
+        tweet_id = int(tweet['data-tweet-id'])
+        if not tweet_id in tweets_in_db:
+          new_tweet = Tweet(id=tweet_id, user=user, tweet_text=tweet['data-tweet-text'])
+          session.add(new_tweet)
+          tweets_in_db.append(tweet_id)
+          session.commit()
+
+       
+      # Insert an Address in the address table
+      # new_address = Address(post_code='00000', person=new_person)
+      # session.add(new_address)
+      # session.commit()
 
     print folder, amt
     
